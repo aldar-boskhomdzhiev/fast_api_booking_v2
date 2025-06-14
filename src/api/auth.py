ad202d24546314f1e0fd5efe98d91e264ae42357
Aldar <aldar08reg@gmail.com>
2025-06-15 00:02:26 +0300

    add method login + generate jwt token

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,22 +1,58 @@
+from datetime import datetime, timezone, timedelta
 
-
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from passlib.context import CryptContext
 from sqlalchemy.exc import IntegrityError
+import jwt
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
 
-router = APIRouter(prefix='/auth', tags=["Авторизация и аутентификация"])
-
+router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@router.post("/login")
+async def login_user(
+        data: UserRequestAdd,
+        response: Response,
+):
+
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail='Пользователь с таким email не зарегистрирован')
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail='Пароль неверный')
+        access_token = create_access_token({"user_id": user.id})
+        response.set_cookie(key="access_token", value=access_token, httponly=True)
+        return {"access_token": access_token}
+
+
+
 
 @router.post("/register")
 async def register_user(
-    data:UserRequestAdd,
+    data: UserRequestAdd,
 ):
     hashed_password = pwd_context.hash(data.password)
 
@@ -28,6 +64,5 @@ async def register_user(
             return {"status": "OK", "data": user}
         except IntegrityError:
             raise HTTPException(
-                status_code=409,
-                detail= "Такой пользователь уже существует"
+                status_code=409, detail="Такой пользователь уже существует"
             )
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from sqlalchemy import select, insert, update, delete
 from pydantic import BaseModel
-from sqlalchemy.exc import IntegrityError
+
 
 
 class BaseRepository:
@@ -24,10 +24,11 @@ class BaseRepository:
         model = result.scalars().one()
         return self.schema.model_validate(model, from_attributes= True )
 
+
     async def get_one_or_none(self, **filter_by):
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
-        model = result.scalars().all().one_or_none()
+        model = result.scalars().one_or_none()
         if model is None:
             return None
         return self.schema.model_validate(model, from_attributes= True )
